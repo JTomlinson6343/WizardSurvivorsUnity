70c751fab40d143beaa8c260f373b1986d2d4cf1
James Tomlinson <jatommo@gmail.com>
2023-06-21 20:45:02 +0100

    Add bar script and resize function

---
+++ Assets/Scripts/BasicBar.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicBar : MonoBehaviour
+{
+    [SerializeField] Vector2 baseScale;
+
+    [SerializeField] float current;
+    [SerializeField] float max;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateSize(current, max);
+    }
+
+    void UpdateSize(float currentValue, float maxValue)
+    {
+        GetComponent<RectTransform>().localScale = new Vector3(currentValue / maxValue * baseScale.x, baseScale.y, 1);
+        //transform.localScale = new Vector3(currentValue/maxValue * baseScale.x, baseScale.y);
+    }
+}
