135508c07e27d435e47472746b4fd7bc7bac08a3
LazyTeaStudios <lazyteastudios@gmail.com>
2023-06-21 23:36:21 +0100

    Animations
    - Player & Enemy Idle/Move

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,10 +7,12 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Rigidbody2D m_RigidBody;
     [SerializeField] private float m_WalkSpeed = 1.0f;
 
+    private Animator m_Animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_Animator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
@@ -28,6 +30,8 @@ public class PlayerMovement : MonoBehaviour
         m_RigidBody.velocity = currentVelocity;
 
         transform.GetComponentInChildren<SpriteRenderer>().flipX = targetVelocity.x < 0;
+
+        SetAnimState();
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -42,5 +46,23 @@ public class PlayerMovement : MonoBehaviour
         currentVelocity -= moveDir * 1.0f;
 
         //m_RigidBody.velocity = currentVelocity;
+    }
+
+    void SetAnimState()
+    {
+        Vector3 currentVelocity = m_RigidBody.velocity;
+
+        if (currentVelocity.magnitude > 0)
+        {
+            // Object is moving
+            m_Animator.SetBool("Moving", true);
+            m_Animator.SetBool("Idle", false); // Disable the idle state
+        }
+        else
+        {
+            // Object is not moving
+            m_Animator.SetBool("Moving", false); // Disable the moving state
+            m_Animator.SetBool("Idle", true);
+        }
     }
 }
