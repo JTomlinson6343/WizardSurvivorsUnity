f595a3b005f181b0ff2282a9ca1087622d735c0d
James Tomlinson <jatommo@gmail.com>
2023-06-22 18:44:26 +0100

    Add icon script

--- Assets/Scripts/AbilityManager.cs
+++ Assets/Scripts/AbilityManager.cs
@@ -10,16 +10,32 @@ public class AbilityManager : MonoBehaviour
 
     AbilityStats m_AbilityStatsBuffs;
 
+    AbilityIcon[] m_Icons;
+
+    bool m_AbilityChoicesShown;
+
     public void Start()
     {
+        m_Icons = GetComponentsInChildren<AbilityIcon>();
+        foreach (AbilityIcon icon in m_Icons)
+        {
+            icon.enabled = false;
+        }
+
         ShowAbilityOptions();
+    }
+
+    public void Update()
+    {
+        if (m_AbilityChoicesShown)
+        {
+
+        }
     }
 
     void ShowAbilityOptions()
     {
         Ability[] displayedAbilities = new Ability[3];
-
-        Image[] icons = GetComponentsInChildren<Image>();
 
         int iconCounter = 0;
 
@@ -35,9 +51,10 @@ public class AbilityManager : MonoBehaviour
                 continue;
             }
             // If all checks pass, set the icon of the UI to the icon of the ability
-            icons[iconCounter].sprite = ability.m_Info.icon;
+            m_Icons[iconCounter].GetComponent<Image>().sprite = ability.m_Info.icon;
+            m_Icons[iconCounter].enabled = true;
+            iconCounter++;
         }
-        icons[0].sprite = m_Abilities[0].m_Info.icon;
     }
     
     bool CheckAlreadyDisplayed(Ability ability, Ability[] displayedAbilities)
@@ -47,10 +64,36 @@ public class AbilityManager : MonoBehaviour
         {
             if (ability == displayedAbility)
             {
-                // If ability is not already shown, test fails
+                // If ability is already shown, test fails
                 return true;
             }
         }
         return false;
+    }
+
+    void HandleInput(KeyCode key)
+    {
+        switch(key)
+        {
+            case KeyCode.UpArrow:
+                AbilityWasSelected(m_Icons[0]);
+                break;
+            case KeyCode.DownArrow:
+                AbilityWasSelected(m_Icons[1]);
+                break;
+            case KeyCode.LeftArrow:
+                AbilityWasSelected(m_Icons[2]);
+                break;
+            case KeyCode.RightArrow:
+                AbilityWasSelected(m_Icons[3]);
+                break;
+            default:
+                break;
+        }
+    }
+
+    void AbilityWasSelected(AbilityIcon icon)
+    {
+
     }
 }
