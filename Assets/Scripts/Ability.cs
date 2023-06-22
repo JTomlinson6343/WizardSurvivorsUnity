425e7520530b1e386965ffc424932cd89c43b99c
James Tomlinson <jatommo@gmail.com>
2023-06-22 14:12:44 +0100

    Add level up logic

--- Assets/Scripts/Ability.cs
+++ Assets/Scripts/Ability.cs
@@ -5,18 +5,61 @@ using UnityEngine;
 
 public struct AbilityStats
 {
-    float damage;
-    float aoe;
-    float duration;
-    float speed;
-    float cooldown;
-    float amount;
-    float knockback;
-    int   pierceAmount;
+    public float aoe;           // Modifier of radius
+    public float duration;      // Durarion in seconds
+    public float damageScaling; // Percentage of player damage dealt by the ability
+    public float speed;         // Speed of projectile/animation of the ability
+    public float cooldown;      // Cooldown in seconds of the ability
+    public float amount;        // Amount of projectiles fired by the ability
+    public float knockback;     // Knockback of the ability
+    public int   pierceAmount;  // Number of enemies that can be pierced
 }
 
 public class Ability : MonoBehaviour
 {
-    AbilityStats m_BaseStats;
-    AbilityStats m_BonusStats;
+    // Level of the ability
+    int m_Level;
+
+    AbilityStats m_BaseStats;   // Base stats of the ability
+    AbilityStats m_BonusStats;  // Bonus stats gained when ability is leveled up
+    AbilityStats m_TotalStats;  // Total combined stats combining base stats, bonus stats and character stats (from buffs)
+
+    void LevelUp()
+    {
+        switch (m_Level)
+        {
+            case 1:
+                Level2();
+                break;
+            case 2:
+                Level3();
+                break;
+            case 3:
+                Level4();
+                break;
+            case 4:
+                Level5();
+                break;
+            default:
+                break;
+        }
+    }
+
+    // Functions called when the ability is upgraded to the specific level
+    protected virtual void Level2()
+    {
+
+    }
+    protected virtual void Level3()
+    {
+
+    }
+    protected virtual void Level4()
+    {
+
+    }
+    protected virtual void Level5()
+    {
+
+    }
 }
