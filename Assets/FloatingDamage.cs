cbac8e563053b9b3f94485cf77d5e964927992ef
James Tomlinson <jatommo@gmail.com>
2023-06-25 18:39:30 +0100

    Set text and colour

--- Assets/FloatingDamage.cs
+++ Assets/FloatingDamage.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class FloatingDamage : MonoBehaviour
@@ -12,26 +13,32 @@ public class FloatingDamage : MonoBehaviour
     private float m_Gradient;
 
     public Color m_Colour;
+    public float m_Damage;
 
     private void Start()
     {
-        transform.localScale = m_StartSize;
-    }
-    private void Awake()
-    {
-        Invoke(nameof(DestroySelf), m_ShrinkTime);
+        transform.localScale = m_StartSize; Invoke(nameof(DestroySelf), m_ShrinkTime);
         m_Gradient = (m_StartSize.x - m_EndSize.x) / m_ShrinkTime;
+        GetComponent<TextMeshPro>().text = DamageToText();
+        GetComponent<TextMeshPro>().color = m_Colour;
     }
     // Update is called once per frame
     void Update()
     {
         if(transform.localScale.x > m_EndSize.x)
         {
+            // Shrink damage numbers
             transform.localScale -= new Vector3(m_Gradient * Time.deltaTime, m_Gradient * Time.deltaTime);
         }
         float risePerFrame = m_RiseSpeed * Time.deltaTime;
 
+        // Make damage numbers rise
         transform.position = new Vector2(transform.position.x, transform.position.y + risePerFrame);
+    }
+
+    private string DamageToText()
+    {
+        return Mathf.RoundToInt(m_Damage).ToString();
     }
 
     private void DestroySelf()
