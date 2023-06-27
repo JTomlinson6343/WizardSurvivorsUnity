c7c9cd64c4b344747ec19d2b8eae3d325b075595
James Tomlinson <jatommo@gmail.com>
2023-06-27 18:06:43 +0100

    If cooldown is -1, only cast the ability once

--- Assets/Scripts/Abilities/Spinning.cs
+++ Assets/Scripts/Abilities/Spinning.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Spinning : Ability
 {
-    public override void OnChosen()
+    public override void OnCast()
     {
         ProjectileManager.m_Instance.ShootMultipleSpinning(m_TotalStats.speed, m_TotalStats.damageScaling, m_TotalStats.AOE, m_TotalStats.amount);
     }
