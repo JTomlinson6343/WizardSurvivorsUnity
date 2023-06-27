9dd8f7f85b9a2990ba0cf85aedb71f18bbec85e0
James Tomlinson <jatommo@gmail.com>
2023-06-27 04:27:54 +0100

    Add functions to update indicators

--- Assets/ProgressionManager.cs
+++ Assets/ProgressionManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
@@ -10,8 +11,13 @@ public class ProgressionManager : MonoBehaviour
 
     BasicBar xpBar;
 
-    float m_Score;
+    [SerializeField] GameObject m_WaveIndicator;
+    [SerializeField] GameObject m_ScoreIndicator;
+    [SerializeField] GameObject m_LevelIndicator;
+
+    float m_Score = 0;
     float m_Level = 1;
+    float m_WaveCounter = 1;
 
     float m_CurrentXP = 0;
     float m_NextLevelXP;
@@ -35,9 +41,23 @@ public class ProgressionManager : MonoBehaviour
 
     }
 
+    public void UpdateWaveIndicator(float wave)
+    {
+        m_WaveIndicator.GetComponent<TextMeshProUGUI>().text = "Wave: " + Mathf.RoundToInt(wave).ToString();
+    }    
+    public void UpdateScoreIndicator(float score)
+    {
+        m_ScoreIndicator.GetComponent<TextMeshProUGUI>().text = "Score: " + Mathf.RoundToInt(score).ToString();
+    }    
+    public void UpdateLevelIndicator(float level)
+    {
+        m_LevelIndicator.GetComponent<TextMeshProUGUI>().text = "Level: " + Mathf.RoundToInt(level).ToString();
+    }
+
     public void AddScore(float score)
     {
-
+        m_Score += score;
+        UpdateScoreIndicator(m_Score);
     }
 
     public bool AddXP(float xp)
