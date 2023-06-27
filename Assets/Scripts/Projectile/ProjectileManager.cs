29428c12ad149326c25d96fcc3503eea3b9eabe7
James Tomlinson <jatommo@gmail.com>
2023-06-27 18:18:58 +0100

    Rewrite projectile manager to remove code duplication

--- Assets/Scripts/Projectile/ProjectileManager.cs
+++ Assets/Scripts/Projectile/ProjectileManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.LowLevel;
 using UnityEngine.Rendering.Universal;
+using static UnityEditor.PlayerSettings;
 
 public enum SpawnPoint
 {
@@ -61,7 +62,21 @@ public class ProjectileManager : MonoBehaviour
         return Player.m_Instance.GetStats().damage;
     }
 
-    public void Shoot(Vector2 pos, Vector2 dir, float speed, float damageScaling, float lifetime)
+    // Function used to spawn bullets at a pre-defined point
+    private Vector2 GetSpawnPoint(SpawnPoint spawnPoint)
+    {
+        switch (spawnPoint)
+        {
+            case SpawnPoint.Player:
+                return Player.m_Instance.GetPosition();
+            case SpawnPoint.Staff:
+                return Player.m_Instance.GetStaffTransform().position;
+            default:
+                return Vector2.zero;
+        }
+    }
+
+    public GameObject Shoot(Vector2 pos, Vector2 dir, float speed, float damageScaling, float lifetime)
     {
         // Create bullet from prefab
         GameObject bullet = Instantiate(m_BulletPrefab);
@@ -78,29 +93,13 @@ public class ProjectileManager : MonoBehaviour
 
         // Rotate projectile in direction of travel
         bullet.transform.eulerAngles = new Vector3(0f, 0f, Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg -90);
+
+        return bullet;
     }
 
-    public void Shoot(SpawnPoint spawnPoint, Vector2 dir, float speed, float damageScaling, float lifetime)
+    public GameObject[] MultiShot(Vector2 pos, float speed, int numShots, float damageScaling, float lifetime)
     {
-        Vector2 pos = Vector2.zero;
-        // Set pos of bullet
-        switch (spawnPoint)
-        {
-            case SpawnPoint.Player:
-                pos = Player.m_Instance.GetPosition();
-                break;
-            case SpawnPoint.Staff:
-                pos = Player.m_Instance.GetStaffTransform().position;
-                break;
-            default:
-                break;
-        }
-        // Call shoot function
-        Shoot(pos,dir,speed,damageScaling, lifetime);
-    }
-
-    public void MultiShot(Vector2 pos, float speed, int numShots, float damageScaling, float lifetime)
-    {
+        GameObject[] bullets = new GameObject[numShots];
         // How many degrees separate each shot
         float interval = 360 / numShots;
 
@@ -114,30 +113,12 @@ public class ProjectileManager : MonoBehaviour
             float x = Mathf.Sin(angle);
             float y = Mathf.Cos(angle);
 
-            Shoot(pos, new Vector2(x,y), speed, damageScaling, lifetime);
+            bullets[i] = Shoot(pos, new Vector2(x,y), speed, damageScaling, lifetime);
         }
+        return bullets;
     }
 
-    public void MultiShot(SpawnPoint spawnPoint, float speed, int numShots, float damageScaling, float lifetime)
-    {
-        // How many degrees separate each shot
-        float interval = 360 / numShots;
-
-        // How far from 0 degrees to start spawning shots
-        float offset = interval / 2;
-
-        for (int i = 0; i < numShots; i++)
-        {
-            float angle = (interval * i + offset) * Mathf.Deg2Rad;
-
-            float x = Mathf.Sin(angle);
-            float y = Mathf.Cos(angle);
-
-            Shoot(spawnPoint, new Vector2(x,y), speed, damageScaling, lifetime);
-        }
-    }
-
-    public void ShootSpinning(float speed, float damageScaling, float offset, float radius)
+    public GameObject ShootSpinning(float speed, float damageScaling, float offset, float radius)
     {
         // Create bullet from prefab
         GameObject bullet = Instantiate(m_SpinningBulletPrefab);
@@ -150,15 +131,21 @@ public class ProjectileManager : MonoBehaviour
         bulletScript.offset = offset;
         bulletScript.radius = radius;
         bulletScript.Init();
+
+        return bullet;
     }
     
-    public void ShootMultipleSpinning(float speed, float damageScaling, float radius, int amount)
+    public GameObject[] ShootMultipleSpinning(float speed, float damageScaling, float radius, int amount)
     {
+        GameObject[] bullets = new GameObject[amount];
+
         float interval = 360 / amount;
         for (int i = 0; i < amount; i++)
         {
             // Create bullet from prefab
-            ShootSpinning(speed, damageScaling, interval*i, radius);
+            bullets[i] = ShootSpinning(speed, damageScaling, interval*i, radius);
         }
+
+        return bullets;
     }
 }
