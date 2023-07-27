5458c6ab840ec07a797f29c5b3f143cfc1fa036f
James Tomlinson <jatommo@gmail.com>
2023-07-27 15:50:09 +0100

    Clicking buttons shows the name and description

--- Assets/Scripts/Skill Tree/SkillTree.cs
+++ Assets/Scripts/Skill Tree/SkillTree.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class SkillTree : MonoBehaviour
 {
-    private static Skill m_CurrentSkill;
+    public Skill m_CurrentSkill;
+
+    [SerializeField] TextMeshProUGUI m_NameLabel;
+    [SerializeField] TextMeshProUGUI m_DescriptionLabel;
 
     public void SetHighlightedSkill(Skill skill)
     {
         m_CurrentSkill = skill;
+        m_NameLabel.text = m_CurrentSkill.m_SkillName;
+        m_DescriptionLabel.text = m_CurrentSkill.m_Description;
     }
 
     public void EnableHighlightedSkill()
     {
 
+    }
+
+    private void Update()
+    {
+        
     }
 }
