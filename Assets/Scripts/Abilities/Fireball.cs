fd4615e62409ee5c9d8d918973d9fb0957e70498
James Tomlinson <jatommo@gmail.com>
2023-08-01 14:24:38 +0100

    Add onmouseinput override for fireball

--- Assets/Scripts/Abilities/Fireball.cs
+++ Assets/Scripts/Abilities/Fireball.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class Fireball : Ability
 {
     [SerializeField] float m_Lifetime;
+    public override void OnMouseInput(Vector2 aimDirection)
+    {
+        ProjectileManager.m_Instance.Shoot(Player.m_Instance.GetStaffTransform().position,
+            Player.m_Instance.GetAimDirection().normalized,
+            m_TotalStats.speed, this, m_Lifetime);
+    }
+
     public override void OnCast()
     {
         ProjectileManager.m_Instance.Shoot(Player.m_Instance.GetStaffTransform().position,
