cbb397b37b47fde26298b6210036622f8dfd93d7
James Tomlinson <jatommo@gmail.com>
2023-08-01 14:25:12 +0100

    Add get closest enemy function

--- Assets/Scripts/Actors/EnemySpawner.cs
+++ Assets/Scripts/Actors/EnemySpawner.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
@@ -7,7 +5,7 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private GameObject m_PlayerReference;
     [SerializeField] private GameObject m_EnemyPrefab;
     [SerializeField] private GameObject m_EnemiesFolder;
-    
+
     private float m_NextSpawn = 0.0f;
     [SerializeField] private float m_SpawnCooldown = 1.0f;
     [SerializeField] private float m_SpawnRadius = 30.0f;
@@ -16,7 +14,7 @@ public class EnemySpawner : MonoBehaviour
     {
         Vector3 playerPos = m_PlayerReference.transform.position;
 
-        Vector3 spawnPos = playerPos + new Vector3(Random.Range(-m_SpawnRadius,m_SpawnRadius), Random.Range(-m_SpawnRadius, m_SpawnRadius),0).normalized * m_SpawnRadius;
+        Vector3 spawnPos = playerPos + new Vector3(Random.Range(-m_SpawnRadius, m_SpawnRadius), Random.Range(-m_SpawnRadius, m_SpawnRadius), 0).normalized * m_SpawnRadius;
 
         return spawnPos;
     }
@@ -24,7 +22,7 @@ public class EnemySpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -47,5 +45,32 @@ public class EnemySpawner : MonoBehaviour
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(m_PlayerReference.transform.position, m_SpawnRadius);
     }
-}
 
+    public Vector2 GetClosestEnemyPos(Vector2 pos)
+    {
+        float minDist = Mathf.Infinity;
+        Enemy closestEnemy = null;
+
+        // Get enemies
+        Enemy[] enemies = GetComponentsInChildren<Enemy>();
+
+        foreach (Enemy enemy in enemies)
+        {
+            // Calculate distance from enemy
+            float dist = Vector2.Distance(pos, enemy.transform.position);
+            if (minDist > dist)
+            {
+                // If enemy is closer than the closest enemy so far, set closest enemy to this
+                minDist = dist;
+                closestEnemy = enemy;
+            }
+        }
+
+        return closestEnemy.transform.position;
+    }
+
+    public Vector2 GetDirectionToEnemy(Vector2 pos)
+    {
+        return (pos - GetClosestEnemyPos(pos).normalized);
+    }
+}
