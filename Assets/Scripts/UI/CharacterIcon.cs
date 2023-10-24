f79cec90b62ad922ce6296fe2eb687ecb081ac9c
James Tomlinson <jatommo@gmail.com>
2023-10-24 03:22:07 +0100

    Add character icon

---
+++ Assets/Scripts/UI/CharacterIcon.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterIcon : MonoBehaviour
+{
+    public bool m_Unlocked;
+
+    public Player m_Character;
+}
