82d0d0ba408c9f7c42c0ea35e1a8e1297c927be6
James Tomlinson <jatommo@gmail.com>
2023-10-26 04:14:34 +0100

    Create fire debuff logic

--- Assets/Scripts/UI/Skill Tree/Skills/FireDebuffSkill.cs
+++ Assets/Scripts/UI/Skill Tree/Skills/FireDebuffSkill.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class FireDebuffSkill : Skill
 {
+    public float m_Damage;
+    public DamageType m_DamageType;
     public override void Init()
     {
         base.Init();
@@ -12,8 +14,10 @@ public class FireDebuffSkill : Skill
 
     public void OnDamageInstance(DamageInstanceData damageInstance)
     {
+        if (damageInstance.user.GetComponent<Actor>().m_ActorType != ActorType.Player) return;
         if (damageInstance.damageType != DamageType.Fire) return;
+        if (damageInstance.isDoT) return;
 
-
+        damageInstance.reciever.AddComponent<Debuff>().Init(5, m_Damage, m_DamageType, damageInstance.user.GetComponent<Actor>().m_ActorType);
     }
 }
