9a788a7c76322dc90eb7906c2c765d7916eb9cf6
James Tomlinson <jatommo@gmail.com>
2023-10-31 22:48:51 +0000

    Add snow particle effect

--- Assets/Scripts/Abilities/Blizzard.cs
+++ Assets/Scripts/Abilities/Blizzard.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class Blizzard : Ability
 {
     GameObject m_AOEObject;
+    [SerializeField] GameObject m_Particles;
     public override void OnCast()
     {
         base.OnCast();
         m_AOEObject = ProjectileManager.m_Instance.SpawnBlizzard(this, m_TotalStats.AOE);
+        m_Particles.transform.SetParent(Player.m_Instance.gameObject.transform, false);
+        m_Particles.transform.position = Player.m_Instance.GetCentrePos();
+        ParticleSystem[] particles = m_Particles.GetComponentsInChildren<ParticleSystem>();
+        foreach (ParticleSystem particle in particles)
+        {
+            ParticleSystem.MainModule mainModule = particle.main;
+            mainModule.startLifetimeMultiplier = m_TotalStats.AOE / 2.2f;
+            particle.gameObject.SetActive(true);
+        }
     }
 }
