19ae875b7e5351f38431a9f38c1022d951f0ca25
James Tomlinson <jatommo@gmail.com>
2023-11-04 13:01:47 +0000

    Remove debug print

--- Assets/Scripts/UI/Skill Tree/Skills/FireSpeedSkill.cs
+++ Assets/Scripts/UI/Skill Tree/Skills/FireSpeedSkill.cs
@@ -24,8 +24,6 @@ public class FireSpeedSkill : CooldownSkill
 
         StartCooldown();
 
-        Debug.Log("Player's speed increased by" + m_BonusSpeed.speed.ToString());
-
         Player.m_Instance.AddTempStats(m_BonusSpeed, m_Duration);
 
         AudioManager.m_Instance.PlaySound(9);
