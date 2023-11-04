29515e4604f398f63a32fcd9f8d3050bd33edd68
James Tomlinson <jatommo@gmail.com>
2023-11-04 02:44:29 +0000

    Rework aoe object

--- Assets/Scripts/Projectile/AOEObject.cs
+++ Assets/Scripts/Projectile/AOEObject.cs
@@ -1,36 +1,11 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AOEObject : MonoBehaviour
+public class AOEObject : Projectile
 {
-    [HideInInspector] public Ability m_AbilitySource;
-
-    private void OnTriggerStay2D(Collider2D collision)
+    protected override void OnEnemyHit(GameObject enemy)
     {
-        if (collision.gameObject.CompareTag("Enemy"))
-        {
-            if (!collision.gameObject.GetComponent<BlizzardDebuff>())
-            {
-                // If enemy is in the zone and doesnt yet have the debuff, add debuff 
-                BlizzardDebuff blizzardDebuff = collision.gameObject.AddComponent<BlizzardDebuff>();
-                blizzardDebuff.Init(-1f, m_AbilitySource.GetTotalStats().damage, m_AbilitySource.m_Info.damageType,Player.m_Instance.gameObject,false,1);
-                blizzardDebuff.m_AbilitySource = m_AbilitySource;
-            }
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Enemy"))
-        {
-            // If enemy leaves zone, remove debuff
-            Debuff debuff = collision.gameObject.GetComponent<BlizzardDebuff>();
-            if (debuff)
-            {
-                Destroy(debuff);
-            }
-        }
+        DamageEnemy(enemy);
     }
 }
