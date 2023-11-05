eaa596b56bb3114582b3a82a11ef46ac69c25833
James Tomlinson <jatommo@gmail.com>
2023-11-05 19:12:04 +0000

    Make flamethrower disabled by default

--- Assets/Scripts/Abilities/Flamethrower.cs
+++ Assets/Scripts/Abilities/Flamethrower.cs
@@ -9,11 +9,13 @@ public class Flamethrower : Ability
     override public void Start()
     {
         base.Start();
+
         m_FlamethrowerObject.transform.SetParent(Player.m_Instance.GetStaffTransform(), false);
         m_FlamethrowerObject.GetComponentInChildren<FlamethrowerAOE>().m_AbilitySource = this;
     }
     private void Update()
     {
+        if (!m_Enabled) return;
         Vector2 vec = Player.m_Instance.GetAimDirection().normalized;
         m_FlamethrowerObject.SetActive(Input.GetMouseButton(0));
         m_FlamethrowerObject.transform.eulerAngles = new Vector3(0f, 0f, Mathf.Atan2(vec.y, vec.x) * Mathf.Rad2Deg - 90);
