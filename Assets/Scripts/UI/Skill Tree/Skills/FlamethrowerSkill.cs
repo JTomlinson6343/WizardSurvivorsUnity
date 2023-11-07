85cb57c0e476bd1da9ff512e3009225a5ba3c88f
James Tomlinson <jatommo@gmail.com>
2023-11-07 04:28:22 +0000

    Inherit bonus stats from fireball ability

--- Assets/Scripts/UI/Skill Tree/Skills/FlamethrowerSkill.cs
+++ Assets/Scripts/UI/Skill Tree/Skills/FlamethrowerSkill.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class FlamethrowerSkill : Skill
 {
+    [SerializeField] Fireball m_FireballRef;
     public override void Init(SkillData data)
     {
         base.Init(data);
 
         Flamethrower flamethrower = AbilityManager.m_Instance.GetComponentInChildren<Flamethrower>();
+        // Inherit bonus stats from fireball
+        flamethrower.AddBonusStats(m_FireballRef.GetBonusStats());
         flamethrower.Enable();
 
         Player.m_Instance.m_ActiveAbility = flamethrower;
