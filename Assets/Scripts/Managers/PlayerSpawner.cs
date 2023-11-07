99f41a8bee13234abc75f82fc0a012ee785594c9
James Tomlinson <jatommo@gmail.com>
2023-11-07 01:50:11 +0000

    Make playerbounds struct

--- Assets/Scripts/Managers/PlayerSpawner.cs
+++ Assets/Scripts/Managers/PlayerSpawner.cs
@@ -2,13 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+struct PlayerBounds
+{
+    public float top;
+    public float bottom;
+    public float left;
+    public float right;
+
+    // Locks gameobject to bounds and outputs true if object is outside bounds
+    public bool Contains(GameObject gameObject)
+    {
+        Vector3 pos = gameObject.transform.position;
+        if (pos.x < left)
+            return false;
+        if (pos.x > right)
+            return false;
+        if (pos.y > top)
+            return false;
+        if (pos.y < bottom)
+            return false;
+        return true;
+    }
+}
+
 public class PlayerSpawner : MonoBehaviour
 {
     public static PlayerSpawner m_Instance;
     public static GameObject m_Character;
     public GameObject m_Camera;
 
-    [SerializeField] Vector2 m_Bounds;
+    [SerializeField] PlayerBounds m_CameraBounds;
+    [SerializeField] PlayerBounds m_WorldBounds;
 
     private void Awake()
     {
@@ -31,10 +56,10 @@ public class PlayerSpawner : MonoBehaviour
 
     private void Update()
     {
-        if (!(Mathf.Abs(Player.m_Instance.transform.position.x) > m_Bounds.x))
+        if (Player.m_Instance.transform.position.x > m_CameraBounds.left && Player.m_Instance.transform.position.x < m_CameraBounds.right)
             m_Camera.transform.position = new Vector3(Player.m_Instance.transform.position.x, m_Camera.transform.position.y, m_Camera.transform.position.z);
 
-        if (!(Mathf.Abs(Player.m_Instance.transform.position.y) > m_Bounds.y))
+        if (Player.m_Instance.transform.position.y > m_CameraBounds.bottom && Player.m_Instance.transform.position.y < m_CameraBounds.top)
             m_Camera.transform.position = new Vector3(m_Camera.transform.position.x, Player.m_Instance.transform.position.y, m_Camera.transform.position.z);
     }
 }
