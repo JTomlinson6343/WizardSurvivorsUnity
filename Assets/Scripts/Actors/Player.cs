39b4918c8d55b08b19df39583713eb4e1a4c76b4
James Tomlinson <jatommo@gmail.com>
2023-11-23 21:35:09 +0000

    Merge player movement into player class

--- Assets/Scripts/Actors/Player.cs
+++ Assets/Scripts/Actors/Player.cs
@@ -63,9 +63,21 @@ public class Player : Actor
     public float m_IFramesTimer;
     private bool m_IsInvincible;
 
+    Vector3 staffStartPos;
+
+    private Rigidbody2D m_RigidBody;
+    private float m_Acceleration = 50.0f;
+
+    [SerializeField] private Animator m_Animator;
+
+    [SerializeField] private Animator m_AnimatorMask;
+    [SerializeField] private SpriteRenderer spriteMask;
+
     private void Awake()
     {
         m_Instance = this;
+        m_RigidBody = GetComponent<Rigidbody2D>();
+        staffStartPos = staffPos.transform.localPosition;
     }
 
     override public void Start()
@@ -80,6 +92,8 @@ public class Player : Actor
 
         base.Update();
 
+        MovementRoutine();
+
         if (Input.GetMouseButton(0))
         {
             float now = Time.realtimeSinceStartup;
@@ -92,6 +106,73 @@ public class Player : Actor
         }
         UpdateStats();
     }
+    private void MovementRoutine()
+    {
+        Vector3 currentVelocity = m_RigidBody.velocity;
+
+        Vector3 moveDir = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+
+        Vector3 targetVelocity = moveDir * GetComponent<Player>().GetStats().speed;
+
+        //currentVelocity = targetVelocity;
+        currentVelocity += (targetVelocity - currentVelocity) * Time.deltaTime * m_Acceleration;
+
+        m_RigidBody.velocity = currentVelocity;
+
+        SetAnimState(targetVelocity);
+
+        UpdateHealthBar();
+    }
+
+    void SetAnimState(Vector3 targetVelocity)
+    {
+        SpriteRenderer sprite = transform.GetComponentInChildren<SpriteRenderer>();
+
+        // If player is moving right, face right
+        if (targetVelocity.x > 0)
+        {
+            sprite.flipX = false;
+            spriteMask.flipX = false;
+            staffPos.transform.localPosition = staffStartPos;
+        }
+        // If player is moving left, face left
+        else if (targetVelocity.x < 0)
+        {
+            sprite.flipX = true;
+            spriteMask.flipX = true;
+            staffPos.transform.localPosition = staffStartPos * new Vector2(-1, 1);
+        }
+
+        if (targetVelocity.magnitude > 0)
+        {
+            // Object is moving
+            m_Animator.SetBool("Moving", true);
+            m_Animator.SetBool("Idle", false); // Disable the idle state
+
+            m_AnimatorMask.SetBool("Moving", true);
+            m_AnimatorMask.SetBool("Idle", false); // Disable the idle state
+        }
+        else
+        {
+            // Object is not moving
+            m_Animator.SetBool("Moving", false); // Disable the moving state
+            m_Animator.SetBool("Idle", true);
+
+            m_AnimatorMask.SetBool("Moving", false);
+            m_AnimatorMask.SetBool("Idle", true); // Disable the idle state
+        }
+    }
+    void UpdateHealthBar()
+    {
+        Actor actorComponent = gameObject.GetComponent<Actor>();
+
+        float health = actorComponent.GetHealthAsRatio();
+
+        BasicBar bar = gameObject.GetComponentInChildren<BasicBar>();
+
+        bar.UpdateSize(health);
+    }
+
     // If actor has i-frames, return false. Else, return true
     override public DamageOutput TakeDamage(float amount)
     {
