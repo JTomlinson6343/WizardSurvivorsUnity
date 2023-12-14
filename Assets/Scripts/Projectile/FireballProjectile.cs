cee72a59bc34a2c9efb34a43478455aa7056d102
James Tomlinson <jatommo@gmail.com>
2023-12-14 03:18:56 +0000

    Add sound effect to fireball explosion

---
+++ Assets/Scripts/Projectile/FireballProjectile.cs
@@ -0,0 +1,11 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class FireballProjectile : AOESpawningProjectile
+{
+    override protected void SpawnAOE()
+    {
+        base.SpawnAOE();
+        AudioManager.m_Instance.PlaySound(2);
+    }
+}
