bd3cfe9aea442e16e8dea165934d06e48c671fd8
James Tomlinson <jatommo@gmail.com>
2023-12-25 19:13:14 +0000

    Add cooldown to xp pickup

--- Assets/Scripts/Pickups/XPPickup.cs
+++ Assets/Scripts/Pickups/XPPickup.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class XPPickup : Pickup
 {
+    static readonly float kSoundEffectCooldown = 0.05f;
+    static float m_LastSoundTime;
+
     override protected void OnPickup()
     {
         ProgressionManager.m_Instance.AddXP(1);
-        AudioManager.m_Instance.PlaySound(1);
+        if (Time.time - m_LastSoundTime > kSoundEffectCooldown)
+        {
+            AudioManager.m_Instance.PlaySound(1);
+            m_LastSoundTime = Time.time;
+        }
         base.OnPickup();
     }
 }
