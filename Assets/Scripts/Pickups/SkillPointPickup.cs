6bd7b0d1eaf6d9d973354270be13fc7f4925e8d2
James Tomlinson <jatommo@gmail.com>
2023-12-25 17:25:29 +0000

    Add sound effect for skill point gain

--- Assets/Scripts/Pickups/SkillPointPickup.cs
+++ Assets/Scripts/Pickups/SkillPointPickup.cs
@@ -7,7 +7,7 @@ public class SkillPointPickup : Pickup
     override protected void OnPickup()
     {
         ProgressionManager.m_Instance.AddSkillPoints(1);
-        AudioManager.m_Instance.PlaySound(1);
+        AudioManager.m_Instance.PlaySound(13);
         base.OnPickup();
     }
 }
