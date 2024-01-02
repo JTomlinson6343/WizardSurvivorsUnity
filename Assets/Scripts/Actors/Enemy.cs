bd0edaec0bd35cc8fdd90761f397a002c53aa7e5
James Tomlinson <jatommo@gmail.com>
2024-01-02 15:17:35 +0000

    Rename champion to champion skeleton

--- Assets/Scripts/Actors/Enemy.cs
+++ Assets/Scripts/Actors/Enemy.cs
@@ -122,11 +122,4 @@ public class Enemy : Actor
         ProgressionManager.m_Instance.IncrementEnemyKills();
         EnemySpawner.m_Instance.IncrementEnemiesKilled();
     }
-
-    public void MakeChampion()
-    {
-        transform.localScale *= 2f;
-        m_MaxHealth *= 2f;
-        GetComponent<SpriteRenderer>().color = Color.red;
-    }
 }
