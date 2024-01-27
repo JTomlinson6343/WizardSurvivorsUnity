2c342fe2942b9714c51263887b0a076fbdb9eb81
James Tomlinson <jatommo@gmail.com>
2024-01-27 17:02:36 +0000

    Add arranged list of skill icons to HUD

---
+++ Assets/Scripts/UI/Skill Tree/SkillPanel.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillPanel : MonoBehaviour
+{
+    public static SkillPanel m_Instance;
+
+    private SkillTree m_SkillTreeRef;
+
+    // Icon arrange constants
+    [SerializeField] float m_ColumnSpacing;
+    [SerializeField] Vector2 m_IconSize;
+    [SerializeField] float m_RowSpacing;
+    [SerializeField] int   m_RowSize;
+
+    [SerializeField] SkillIcon[] m_Icons;
+
+    private void Awake()
+    {
+        m_Instance = this;
+    }
+
+    public void Init(SkillTree skillTree)
+    {
+        m_SkillTreeRef = skillTree;
+    }
+
+    private void Update()
+    {
+        ArrangeIcons(m_Icons);
+    }
+
+    private void ArrangeIcons(SkillIcon[] icons)
+    {
+        int column = 0;
+        int row = 0;
+        for (int i = 0; i < icons.Length; i++)
+        {
+            icons[i].GetComponent<RectTransform>().anchoredPosition = new Vector2(column * (m_ColumnSpacing + m_IconSize.x), -row * (m_RowSpacing + m_IconSize.y));
+
+            column++;
+            if (column >= m_RowSize)
+            {
+                row++;
+                column = 0;
+            }
+        }
+    }
+}
