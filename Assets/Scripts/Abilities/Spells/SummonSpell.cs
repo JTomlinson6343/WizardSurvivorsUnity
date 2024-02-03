f17a86c64e9588927d638ed49a730259c712da4a
James Tomlinson <jatommo@gmail.com>
2024-02-03 15:31:01 +0000

    Add summon ice elemental spell

---
+++ Assets/Scripts/Abilities/Spells/SummonSpell.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SummonSpell : Spell
+{
+    [SerializeField] GameObject m_SummonPrefab;
+
+    [SerializeField] GameObject m_SmokePrefab;
+    public override void OnCast()
+    {
+        base.OnCast();
+
+        GameObject summon = Instantiate(m_SummonPrefab);
+        GameObject smoke = Instantiate(m_SmokePrefab);
+
+        summon.transform.position = Player.m_Instance.transform.position;
+        smoke.transform.position = summon.transform.position;
+
+        smoke.transform.localScale = summon.transform.localScale * 2f;
+
+        summon.GetComponent<Summon>().m_AbilitySource = this;
+    }
+}
