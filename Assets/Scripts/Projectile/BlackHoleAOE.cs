4198332e90810d1ee37ebe28479f76f5542e90e8
James Tomlinson <jatommo@gmail.com>
2024-02-15 16:14:11 +0000

    Fix particles scaling with aoe size

--- Assets/Scripts/Projectile/BlackHoleAOE.cs
+++ Assets/Scripts/Projectile/BlackHoleAOE.cs
@@ -1,11 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 public class BlackHoleAOE : DebuffAOE
 {
     readonly float kPullSpeedConst = 0.01f;
     readonly float kHitboxShinkAmount = 0.3f;
+    readonly float kParticlesToAOERatio = 0.5f;
     [SerializeField] float m_SuckSpeed;
 
     protected override void OnTriggerStay2D(Collider2D collision)
@@ -18,5 +18,16 @@ public class BlackHoleAOE : DebuffAOE
         Vector3 towardsCentre = (transform.position - collision.transform.position).normalized;
 
         collision.gameObject.transform.position += towardsCentre * kPullSpeedConst * m_SuckSpeed;
+    }
+
+    public override void Init(Vector2 pos, Ability ability, float lifetime)
+    {
+        base.Init(pos, ability, lifetime);
+
+        ParticleSystem particles = GetComponentInChildren<ParticleSystem>();
+        if (!particles) return;
+
+        ShapeModule shape = particles.shape;
+        shape.radius = m_AbilitySource.GetTotalStats().AOE * kParticlesToAOERatio;
     }
 }
