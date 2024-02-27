d138082fd14d73071eb7ee57278c67436b11871f
James Tomlinson <jatommo@gmail.com>
2024-02-27 01:21:42 +0000

    Reduce pickup sound effect sound

--- Assets/Scripts/Pickups/SkillPointPickup.cs
+++ Assets/Scripts/Pickups/SkillPointPickup.cs
@@ -7,7 +7,7 @@ public class SkillPointPickup : Pickup
     override protected void OnPickup()
     {
         ProgressionManager.m_Instance.AddSkillPoints(1);
-        AudioManager.m_Instance.PlaySound(13);
+        AudioManager.m_Instance.PlaySound(13, 0.5f);
         base.OnPickup();
     }
 }
