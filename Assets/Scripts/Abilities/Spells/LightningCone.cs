91137b0776d72fcda05076157bd6ed0cad4762e4
James Tomlinson <jatommo@gmail.com>
2024-02-28 00:12:41 +0000

    Add sound effect to cone

--- Assets/Scripts/Abilities/Spells/LightningCone.cs
+++ Assets/Scripts/Abilities/Spells/LightningCone.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class LightningCone : Spell
 {
@@ -19,16 +20,25 @@ public class LightningCone : Spell
         }
 
         Vector2 dir = GameplayManager.GetDirectionToGameObject(Player.m_Instance.GetStaffTransform().position, closestEnemy);
+
+        ShootCone(dir);
     }
 
     public override void OnMouseInput(Vector2 aimDirection)
+    {
+        ShootCone(aimDirection);
+    }
+
+    private void ShootCone(Vector2 dir)
     {
         GameObject cone = Instantiate(m_LightningCone);
 
         cone.GetComponent<AOEObject>().StartLifetimeTimer(m_Lifetime);
         cone.GetComponent<AOEObject>().m_AbilitySource = this;
-        cone.transform.eulerAngles = new Vector3(0f, 0f, Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90);
-        cone.transform.SetParent(Player.m_Instance.GetStaffTransform());
+        cone.transform.eulerAngles = new Vector3(0f, 0f, Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90);
+        cone.transform.SetParent(Player.m_Instance.transform);
         cone.transform.position = Player.m_Instance.GetStaffTransform().position;
+
+        AudioManager.m_Instance.PlaySound(24, 0.2f);
     }
 }
