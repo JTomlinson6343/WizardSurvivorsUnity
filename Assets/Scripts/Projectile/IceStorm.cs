0749153d06d82d488d2baa233f37dd0d886cca7d
James Tomlinson <jatommo@gmail.com>
2024-02-29 14:49:19 +0000

    Add ice mage basic attack

---
+++ Assets/Scripts/Projectile/IceStorm.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+
+public class IceStorm : ConstantDamageAOE
+{
+    [SerializeField] float m_SlowSpeedModifier;
+    bool m_IsSlowed;
+
+    public override void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Enemy")) return;
+
+        base.OnTriggerEnter2D(collision);
+
+        if (m_IsSlowed) return;
+
+        GetComponent<Rigidbody2D>().velocity *= m_SlowSpeedModifier;
+
+        m_IsSlowed = true;
+    }
+}
