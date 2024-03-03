73e5b963a6a793014341ecb9781873d4453dbc03
James Tomlinson <jatommo@gmail.com>
2024-03-03 21:50:13 +0000

    Add all stat buff ice mage skills

--- Assets/Scripts/UI/Skill Tree/Skills/PlayerBuffSkill.cs
+++ Assets/Scripts/UI/Skill Tree/Skills/PlayerBuffSkill.cs
@@ -6,10 +6,15 @@ public class PlayerBuffSkill : Skill
 {
     [SerializeField] PlayerStats m_StatBuffs;
 
+    [SerializeField] float m_ResistanceBuffs;
+
     public override void Init(SkillData data)
     {
         base.Init(data);
         for (int i = 0; i < data.level; i++)
+        {
             Player.m_Instance.AddBonusStats(m_StatBuffs);
+            Player.m_Instance.m_DamageResistance += m_ResistanceBuffs;
+        }
     }
 }
