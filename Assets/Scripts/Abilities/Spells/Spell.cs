2eca0402dab3b4aa4f867f4f25397a547e8ac848
James Tomlinson <jatommo@gmail.com>
2024-03-07 16:18:52 +0000

    Add spell tag to spells

--- Assets/Scripts/Abilities/Spells/Spell.cs
+++ Assets/Scripts/Abilities/Spells/Spell.cs
@@ -1,9 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Spell : Ability
 {
+    public enum SpellTag
+    {
+        Offensive,
+        Defensive,
+        Summon,
+        Stun
+    }
+
+    [SerializeField] SpellTag[] m_SpellTags;
+
+    public bool HasTag(SpellTag tag)
+    {
+        if (m_SpellTags == null) return false;
+
+        if (m_SpellTags.Contains(tag)) return true;
+
+        return false;
+    }
+
     public override void OnChosen()
     {
         base.OnChosen();
