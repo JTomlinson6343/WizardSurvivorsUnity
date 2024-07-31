7526835a35102b60226095bf6b4504cc69260abf
James Tomlinson <jatommo@gmail.com>
2024-07-31 21:52:44 +0100

    Finish sunbeam spell

---
+++ Assets/Scripts/Abilities/Spells/SunBeam.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunBeam : AOESpawningSpell
+{
+    public override void Start()
+    {
+        base.Start();
+        m_CastAmount = m_BaseCastAmount;
+    }
+
+    protected override void PlaySound()
+    {
+        AudioManager.m_Instance.PlaySound(28);
+    }
+}
