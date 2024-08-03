31ce9e81fe0119374242c2adc0496bace6a54ab9
James Tomlinson <jatommo@gmail.com>
2024-08-03 17:34:43 +0100

    Add cursed ground vfx

---
+++ Assets/Scripts/Abilities/Debuffs/CurseDebuff.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CurseDebuff : SlowDebuff
+{
+    public CurseDebuff(DebuffType type, DamageType damageType, float damage, float maxStacks, float duration, GameObject source, Ability abilitySource, float slowAmount)
+        : base(type, damageType, damage, maxStacks, duration, source, slowAmount) { }
+
+    public override void OnApply(Actor actor)
+    {
+        base.OnApply(actor);
+        // Reduce speed
+        actor.GetComponent<Enemy>().m_DamageResistance -= 0.3f;
+    }
+
+    public override void OnEnd(Actor actor)
+    {
+        actor.GetComponent<Enemy>().m_DamageResistance += 0.3f;
+    }
+}
--- Assets/Scripts/Abilities/Debuffs/Debuff.cs
+++ Assets/Scripts/Abilities/Debuffs/Debuff.cs
@@ -10,7 +10,8 @@ public class Debuff
         Frozen,
         FrozenSkill,
         Paralysed,
-        Frostbite
+        Frostbite,
+        Curse
     }
 
     // Consts
