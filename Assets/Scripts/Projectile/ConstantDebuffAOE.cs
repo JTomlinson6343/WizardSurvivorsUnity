2e73cd79e116c7892cb22951af76dcfe2662ec2f
James Tomlinson <jatommo@gmail.com>
2024-08-03 22:05:49 +0100

    Adjust values

--- Assets/Scripts/Projectile/ConstantDebuffAOE.cs
+++ Assets/Scripts/Projectile/ConstantDebuffAOE.cs
@@ -7,6 +7,11 @@ public class ConstantDebuffAOE : ConstantDamageAOE
 {
     protected Debuff m_DebuffData;
 
+    public override void Init(Vector2 pos, Vector2 dir, float speed, Ability ability, float lifetime)
+    {
+        base.Init(pos, ability, lifetime);
+    }
+
     protected override void DamageTarget(GameObject enemy)
     {
         DebuffManager.m_Instance.AddDebuff(enemy.GetComponent<Actor>(), m_DebuffData);
