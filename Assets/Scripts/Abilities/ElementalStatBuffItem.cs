6ddcbc5e648d124ab9cb6b3b612423d216702b47
James Tomlinson <jatommo@gmail.com>
2024-11-09 18:21:08 +0000

    Rework food items

---
+++ Assets/Scripts/Abilities/ElementalStatBuffItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ElementalStatBuffItem : Item
+{
+    [System.Serializable]
+    struct ElementalAbilityStats
+    {
+        public DamageType damageType;
+        public AbilityStats stat;
+    }
+    [SerializeField] ElementalAbilityStats[] m_ElementalAbilityStatBuffs;
+
+    public override void LevelUp()
+    {
+        foreach (ElementalAbilityStats eStats in m_ElementalAbilityStatBuffs)
+        {
+            AbilityManager.m_Instance.AddElementalAbilityBonusStats(eStats.damageType, eStats.stat);
+        }
+
+        base.LevelUp();
+    }
+}
